7ee51744079910fcdc399dd7e7d444b784aaa751
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-08-26 16:28:28 +0600

    winner selection added

--- index.js
+++ index.js
@@ -10,10 +10,33 @@
 
 
 
-  let randomNumbers2 = Math.floor(Math.random()*6) +1 ;
+  let randomNumber2 = Math.floor(Math.random()*6) +1 ;
 
  
  
-  let randomImageSource2 = "images/dice" + randomNumbers2  + ".png"  ;
+  let randomImageSource2 = "images/dice" + randomNumber2  + ".png"  ;
 
   document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+//  let randomNumber1 = Math.floor(Math.random()*6) +1 ;
+
+//  let randomImageSource = "images/dice" +randomNumber1 + ".png";
+//   document.querySelectorAll("img")[0].setAttribute("src", randomImageSource);
+
+
+
+//    let randomNumbers2 = Math.floor(Math.random()*6) + 1;
+//    let randomImageSource2 = "images/dice"  +randomNumbers2  + ".png";
+//    document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+ if( randomNumber1 > randomNumber2){
+     document.querySelector("h1").innerHTML = "🚩player 1 wins!"
+
+ }
+ else if(randomNumber1 < randomNumber2){
+  document.querySelector("h1").innerHTML = "🚩player 2 wins!"
+ }
+ else{
+  document.querySelector("h1").innerHTML = "Draw!"
+ }
