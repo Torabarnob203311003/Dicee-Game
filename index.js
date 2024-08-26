5935d5f7520bea4460ea4115ef6344d95626691e
Torabarnob203311003 <118876454+maxarnob@users.noreply.github.com>
2024-08-26 16:05:49 +0600

    random dicee  generate added

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-alert("working !");
+ let randomNumber1 =Math.floor(Math.random()*6)+1;// thats will generate random number 1-5
+
+ let randomDiceeImage = "dice"  + randomNumber1 + ".png";// dicee1.png - dicee6.png
+
+ let randomImageSource = "images/" + randomDiceeImage;  //src/dicee1.png - src/dicee6.png
+
+ let image1 = document.querySelectorAll("img")[0];
+ 
+ image1.setAttribute("src", randomImageSource);
+
+
+
+  let randomNumbers2 = Math.floor(Math.random()*6) +1 ;
+
+ 
+ 
+  let randomImageSource2 = "images/dice" + randomNumbers2  + ".png"  ;
+
+  document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
